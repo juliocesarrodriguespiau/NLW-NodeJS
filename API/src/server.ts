d1938f927b61009980c139504dbcc0d340cb07d7
Júlio César Rodrigues Piau <39218054+juliocesarrodriguespiau@users.noreply.github.com>
2021-02-23 00:04:46 -0300

    commit

--- API/src/server.ts
+++ API/src/server.ts
@@ -1,6 +1,31 @@
-import express from 'express';
+import express, { request, response } from 'express';
 
 const app = express();
+
+/**
+ * GET - Buscar
+ * POST - Salvar
+ * PUT - Alterar
+ * DELETE => Deletar
+ * PATCH => Alteração específica
+ */
+
+// http://localhost:3333/users
+app.get("/users", (request, response) => {
+    return response.json({
+        message: "Hello Word - NLW#04"
+    });
+})
+
+// 1º parâmetro => Rota(Recurso API)º
+// 2º parâmetro => request, response
+
+app.post("/", (request, response) => {
+    // recebi os dados para salvar
+    return response.json({
+        message: "Os dados foram salvos com Sucesso!!!"
+    });  
+})
 
 app.listen(3333, () => console.log("Server is running!!!"));
 
