4d88472ea5ba875246a9a0efe765a18bb67fd410
Júlio César Rodrigues Piau <39218054+juliocesarrodriguespiau@users.noreply.github.com>
2021-02-28 11:19:23 -0300

    commit

--- API/src/routers.ts
+++ API/src/routers.ts
@@ -1,10 +1,16 @@
 import { Router } from 'express';
+import { SurveysController } from './controllers/SurveysController';
 import { UserController } from './controllers/UserController';
+import { Survey } from './models/Survey';
+import { SurveysRepository } from './repositories/SurveysRepository';
 
  const router = Router();
 
  const userController = new UserController();
+ const surveysController = new SurveysController();
 
  router.post("/users", userController.create);
+ router.post("/surveys", surveysController.create);
+ router.get("/surveys", surveysController.show);
 
  export { router };
