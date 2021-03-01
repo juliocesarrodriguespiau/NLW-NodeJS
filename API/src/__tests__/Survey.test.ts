e587a46f0368298a23f3e4317bd64961d143f3bf
Júlio César Rodrigues Piau <39218054+juliocesarrodriguespiau@users.noreply.github.com>
2021-03-01 20:38:06 -0300

    ultimo commit

---
+++ API/src/__tests__/Survey.test.ts
@@ -0,0 +1,20 @@
+import request from 'supertest';
+import { app } from "../app";
+
+import createConnection from "../database";
+
+describe("Surveys", async () => {
+    beforeAll(async () => {
+        const connection = await createConnection();
+        await connection.runMigrations();
+    });
+
+    it("Should be able to create a new survey", async () => {
+        const response = await request(app).post("/surveys").send({
+            title: "Title Example",
+            description: "Description Example",
+        });
+
+        expect(response.status).toBe(201);
+    });
+});
